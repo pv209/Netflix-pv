150c84ad0e9544aa10b60941ebb6899a5373f457
Paulo <pvtrybe209@gmail.com>
2023-08-21 15:31:54 -0300

    iniciando site netflix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import './App.css'
+import Api from './Api.js';
+import MovieRow from './componnents/MovieRow.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default () => {
+
+  const [movieList, setMovieList] = useState ([]);
+    useEffect(() => {
+      const loadAll  = async () => {
+        let list =  await Api.getHomeList();
+        setMovieList(list);
+      }
+      loadAll();
+    },[]);
+
+      return (
+        <div className='page'>
+          <section className="lists">
+            {movieList.map((item,key) => (
+              <div>
+                <MovieRow key={key} title={item.title} items={item.items}/>
+              </div>
+          ))}
+          </section>
+        </div>
+      );
 }
-
-export default App;
